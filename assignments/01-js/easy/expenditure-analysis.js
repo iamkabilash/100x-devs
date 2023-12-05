0d6775b5094513ef8ad2de50e5d8ed955e97aef3
Kabilash Satheesh Kumar <iamkabilash@gmail.com>
2023-12-05 12:49:58 +0530

    01-assignment wip

---
+++ assignments/01-js/easy/expenditure-analysis.js
@@ -0,0 +1,70 @@
+/*
+  Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
+  and return a list of objects where each object is unique category-wise and has total price spent as its value.
+  Transaction - an object like { itemName, category, price, timestamp }.
+  Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
+*/
+
+function calculateTotalSpentByCategory(transactions) {
+  const output = [];
+
+  transactions.forEach((transaction) => {
+    if (output.length === 0) {
+      let obj = {};
+      obj[transaction][category] = transaction.price;
+      output.push(obj);
+    }
+    output.forEach((item) => {
+      if (item.category === transaction[transaction][category]) {
+        item[transaction][category] += transaction.price;
+      } else {
+        let obj = {};
+        obj[transaction][category] = transaction.price;
+        output.push(obj);
+      }
+    });
+  });
+
+  console.log(output);
+  return 1;
+}
+
+calculateTotalSpentByCategory([
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: "Food",
+    itemName: "Burger",
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: "Clothing",
+    itemName: "T-Shirt",
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: "Electronics",
+    itemName: "Headphones",
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: "Clothing",
+    itemName: "Jeans",
+  },
+]);
+
+module.exports = calculateTotalSpentByCategory;
